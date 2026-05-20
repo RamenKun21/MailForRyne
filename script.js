1d2584949e454023349bfb5606afc4c9c763c44a
KRXZY <slurptheramen21@gmail.com>
2026-05-20 22:33:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const gifStages = [
-     "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
-    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
+     "https://media1.tenor.com/m/MKkJWYigjycAAAAC/cute-cat-cat-cute.gif",    // 0 normal
+    "https://media1.tenor.com/m/ZsGv52SY0OAAAAAC/zuh-cat.gif",  // 1 confused
+    "https://media1.tenor.com/m/YWRLnlD8TxIAAAAC/cat-please.gif",             // 2 pleading
+    "https://media1.tenor.com/m/67hJkk5s-H4AAAAC/crying-crying-cat.gif",             // 3 sad
+    "https://media1.tenor.com/m/0QkZnCmGFX0AAAAC/banana-cat-banana-cat-crying.gif",       // 4 sadder
+    "https://media1.tenor.com/m/I6WG5aQzlWcAAAAC/very-sad-cat-sad-cat.gif",             // 5 devastated
+    "https://media1.tenor.com/m/YYtWpKR5-qEAAAAd/fat-cat-vanished.gif",               // 6 very devastated
+    "https://media1.tenor.com/m/oObwAjW2IIoAAAAd/cat-cute-cat.gif"  // 7 crying runaway
 ]
 
 const noMessages = [
